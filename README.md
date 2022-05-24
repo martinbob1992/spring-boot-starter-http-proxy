2ba49367b2fb21b16d5f70f589dc85875f40daf6
Marsh <610385220@qq.com>
2022-05-24 17:57:05 +0800

    新增readme.md

---
+++ README.md
@@ -0,0 +1,15 @@
+# spring-boot-starter-http-proxy
+
+
+#### 组件简介
+
+> 组件参考mybatis Mapper源代码灵感而来，主要为了减轻接口对接工作复杂度
+
+## Demo使用说明
+代码使用参照test目录下的demo进行
+
+![](https://github.com/martinbob1992/spring-boot-starter-http-proxy/blob/master/src/main/images/test.png?raw=true)
+
+![](https://github.com/martinbob1992/spring-boot-starter-http-proxy/blob/master/src/main/images/test1.png?raw=true)
+
+![](https://github.com/martinbob1992/spring-boot-starter-http-proxy/blob/master/src/main/images/test2.png?raw=true)
--- src/test/java/com/marsh/demo/proxy/TestProxy.java
+++ src/test/java/com/marsh/demo/proxy/TestProxy.java
@@ -4,7 +4,7 @@ import cn.hutool.http.Method;
 import com.marsh.demo.actuator.TestDataProxy;
 import com.marsh.demo.convert.DataResponseConvert;
 import com.marsh.demo.convert.DataResult;
-import com.marsh.proxy.annotations.RequestQuery;
+import com.marsh.proxy.annotations.*;
 import com.marsh.proxy.binding.DataProxy;
 import com.marsh.proxy.convert.ResponseHandler;
 
@@ -19,6 +19,12 @@ public interface TestProxy {
 
     @TestDataProxy(url= "/test",method = Method.GET,key = "get")
     @ResponseHandler(convert = DataResponseConvert.class)
+    /**
+     * @see RequestQuery
+     * @see RequestBody
+     * @see RequestHeader
+     * @see RequestFromData
+     */
     DataResult testGet(@RequestQuery(name = "msg") String msg);
 
     @TestDataProxy(url= "/test",method = Method.POST,key = "post")
